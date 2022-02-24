a11b75c0a8c13bb78c96064dd6966ac2dfbac29f
porterlin <porter0418@gmail.com>
2022-02-24 21:53:13 +0800

    fifth version

--- main.py
+++ main.py
@@ -177,6 +177,10 @@ def job():
             time.sleep(5)
     
             driver.get_screenshot_as_file("test.jpg")
+            
+            for i in range(3):
+                driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+                time.sleep(5)
 
             Soup = BeautifulSoup(driver.page_source, 'html.parser')
         
